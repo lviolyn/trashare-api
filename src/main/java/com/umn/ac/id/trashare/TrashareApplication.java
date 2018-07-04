ce801c809287d3fc7dcd4cf93c1e6d24735af0f1
Kevin Alexander <kevin.alexander@student.umn.ac.id>
2018-07-04 16:56:56 +0700

    Swagger

--- src/main/java/com/umn/ac/id/trashare/TrashareApplication.java
+++ src/main/java/com/umn/ac/id/trashare/TrashareApplication.java
@@ -2,9 +2,25 @@ package com.umn.ac.id.trashare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TrashareApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("POST","PUT","DELETE","GET","OPTIONS")
+                        .allowCredentials(false);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TrashareApplication.class, args);
